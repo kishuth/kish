308bb906a1321fcb15f47fd5f389fe5be18920c2
kishuth <ruthkiniaru2016@gmail.com>
2025-08-19 18:42:59 +0300

    Add files via upload

---
+++ MY LIST.py
@@ -0,0 +1,23 @@
+# QUESTIONS
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+# QUESTION 2
+my_list.extend([50, 60, 70])
+
+# QUESTION 3
+my_list.pop()
+
+# QUESTION 4
+my_list.sort()
+
+# QUESTION 5
+index_30 = my_list.index(30)
+
+# QUESTION 6
+print("Final list:", my_list)
+print("Index of 30:", index_30)
